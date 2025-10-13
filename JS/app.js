4440f7932f26080e1dc7ec34f4890a700cf24c40
Danareynolds-coding <dana.reynolds@mscoding.org>
2025-10-13 08:37:18 -0500

    update

---
+++ JS/app.js
@@ -0,0 +1,7 @@
+ document.getElementById('itemTotal').innerHTML =
+  'Item (' + (1 + 1) + '): $ ' + ((2095 + 799) / 100);
+document.getElementById('shipping').innerHTML =
+`Shipping and handling: ${shipping}`;
+document.getElementById('beforeTax').innerHTML =
+`Total before tax: ${beforeTax}`;
+
